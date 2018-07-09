c3b2511683d319256276b11e6675a2061f2ecfb8
yasas1 <32437531+yasas1@users.noreply.github.com>
2018-07-09 20:16:10 +0530

    updated

--- LabReservation/frontend/src/app/reservations.service.ts
+++ LabReservation/frontend/src/app/reservations.service.ts
@@ -20,7 +20,7 @@ export class ReservationsService {
     }).pipe(map((res:any)  => res));
   }
 
-  /*
+  
   checkAvailable(lab,date,stime,etime){
 
     return this._http.get('http://127.0.0.1:3000/reservations/checkAvailable/'+lab+'/'+date+'/'+stime+'/'+etime,{
@@ -30,7 +30,7 @@ export class ReservationsService {
       headers:new HttpHeaders().append('Content-Type','application/json')
     }).pipe(map((res:any)  => res));
   }
-
+/*
   countlab(date){
     return this._http.get('http://127.0.0.1:3000/reservations/getlabrescount/'+date,{
       observe:'body',
